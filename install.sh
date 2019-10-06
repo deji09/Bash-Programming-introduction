87292c26a57ab5bf011d1a6094967eff381e3db0
Max Kelly <maxwill.kelly@gmail.com>
2019-10-06 01:17:34 +0100

    Global install enabled, minor changes

--- install.sh
+++ install.sh
@@ -6,14 +6,30 @@ echo "With this installer there are two types of installation"
 echo "For more information please read the README file on Github"
 echo "Please select your preferred option:"
 
+system_user=$(whoami)
+
+function globalInstall() {
+    local installPath=/usr/local/bin/trackpro
+    local configPath=/etc/trackpro.conf
+    echo
+    if [ "$system_user" == "root" ]; then
+        mkdir $installPath
+        cp -v ./source/trackpro.sh $installPath/trackpro
+        cp -vr ./source/scripts $installPath
+        cp -v ./source/config/trackpro.conf $configPath
+        echo
+    else 
+        echo "Install aborted: Superuser privleges required"
+    fi
+}
+
 PS3='Please enter your choice: '
 options=("Global" "Local" "Exit Installation")
 select opt in "${options[@]}"
 do
     case $opt in
         "Global")
-            echo
-            echo "Global"
+            globalInstall
             break
             ;;
         "Local")
@@ -30,7 +46,3 @@ do
         *) echo "invalid option $REPLY";;
     esac
 done
-
-function global_install() {
-
-}
