ca22686ddd977308d3212bfba253c9ddc9a0afe5
Max Kelly <maxwill.kelly@gmail.com>
2019-10-07 17:40:30 +0100

    Main control flow for uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+# trackpro Uninstall script
+
+echo "Welcome to the trackpro uninstaller"
+echo "We are sad you are leaving us"
+
+read -p "Are you sure you want to uninstall trackpro [N/y] " yn
+case $yn in
+    [Yy]* )
+        echo "Uninstall started"
+        exit
+    ;;
+    * )
+        echo "Uninstall canceled"
+        exit
+    ;;
+esac
