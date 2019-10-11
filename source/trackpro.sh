247d9bce1fc218e47f25ad5669113a42dddfd22a
Max Kelly <maxwill.kelly@gmail.com>
2019-10-11 21:07:37 +0100

    Handles longfrom args again

--- source/trackpro.sh
+++ source/trackpro.sh
@@ -5,10 +5,6 @@ version=0.0
 # Variables to set user arguments for debugging
 # $1 = -c
 
-# Changes to the absolute path of the script
-trackproPath=`dirname "$0"`
-trackproPath=`( cd "$trackproPath" && pwd )`
-
 # Sets the configuration path and imports its variables
 setConfigPath() {
     local localConfigPath=$HOME/.trackpro/trackpro.conf
@@ -26,59 +22,136 @@ setConfigPath() {
     elif [ -f "$sourceConfigPath" ]; then
         source $sourceConfigPath
         configPath=$sourceConfigPath
-    else 
+    else
         echo "Error: No valid configuration file found"
     fi
 }
 
-# Displays the welcome message
-echo "Welcome to trackpro (version $version)"
-# Sets the configuration file and imports its variables
-setConfigPath
-# 
-if [ "$1" == "" ]; then
-    echo "Error: Option argument required"
-    source $trackproPath/scripts/help.sh;
-fi
-# 
-if [ "$2" == "" ]; then
-    target=null
-else 
-    target=$2
-fi
-# Interprets first argument
-while getopts "chmlstu" opt; do
-    case ${opt} in
-        c )
+# Interprets first longform argument
+longForm() {
+    case "$1" in
+        "--changesettings")
             echo changesettings
             source $trackproPath/scripts/changesettings.sh $configPath
         ;;
-        h )
+        "--displayhelp")
             echo displayhelp;
             source $trackproPath/scripts/help.sh;
         ;;
-        m )
+        "--makerepo")
             echo makerepo
             source $trackproPath/scripts/makerepo.sh $target $configPath;
         ;;
-        l )
-            echo listrepos $repoPaths
+        "--listrepos")
+            echo listrepos
             source $trackproPath/scripts/listrepos.sh $repoPaths;
         ;;
-        s )
+        "--storechanges")
             echo storechanges
             source $trackproPath/scripts/storechanges.sh $target $repoPaths;
         ;;
-        t )
-            echo tar 
+        "--tar")
+            echo tar
             source $trackproPath/scripts/tar.sh $target $configPath;
         ;;
-        u )
+        "--undochange")
             echo undochange
             source $trackproPath/scripts/undochange.sh $target $configPath;
         ;;
         * )
+            echo "Error: illegal option -$1"
             source $trackproPath/scripts/help.sh;
     esac
-done
+}
+
+# Interprets first shorthand argument
+shortForm() {
+    while getopts "chmlstu" opt; do
+        case ${opt} in
+            c )
+                echo changesettings
+                source $trackproPath/scripts/changesettings.sh $configPath
+            ;;
+            h )
+                echo displayhelp;
+                source $trackproPath/scripts/help.sh;
+            ;;
+            m )
+                echo makerepo
+                source $trackproPath/scripts/makerepo.sh $target $configPath;
+            ;;
+            l )
+                echo listrepos
+                source $trackproPath/scripts/listrepos.sh $repoPaths;
+            ;;
+            s )
+                echo storechanges
+                source $trackproPath/scripts/storechanges.sh $target $repoPaths;
+            ;;
+            t )
+                echo tar
+                source $trackproPath/scripts/tar.sh $target $configPath;
+            ;;
+            u )
+                echo undochange
+                source $trackproPath/scripts/undochange.sh $target $configPath;
+            ;;
+            * )
+                source $trackproPath/scripts/help.sh;
+        esac
+    done
+}
+
+# Changes to the absolute path of the script, makes importing other scripts easier
+absolutePath() {
+    trackproPath=`dirname "$0"`
+    trackproPath=`( cd "$trackproPath" && pwd )`
+}
+
+configureArgs() {
+    #
+    if [ "$1" == "" ]; then
+        echo "Error: Option argument required"
+        source $trackproPath/scripts/help.sh;
+    elif [[ "$1" == *"--"* ]]; then
+        short=false
+        long=true
+    elif [[ "$1" == *"-"* ]]; then
+        short=true
+        long=false
+    else
+        short=false
+        long=false
+    fi
+    #
+    if [ "$2" == "" ]; then
+        target=null
+    else
+        target=$2
+    fi
+}
+
+main() {
+    # Displays the welcome message
+    echo "Welcome to trackpro (version $version)"
+    #
+    absolutePath
+    # Sets the configuration file and imports its variables
+    setConfigPath
+    #
+    configureArgs $1 $2
+    if [ "$long" == "true" ]; then
+        #
+        longForm $1
+    elif [ "$short" == "true" ]; then
+        #
+        shortForm $1
+    else
+        echo "Error: illegal option -$1"
+        source $trackproPath/scripts/help.sh;
+    fi
+}
+
+main $1 $2
+
 
