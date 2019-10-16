9b6eca8b9745cf247cd2983aa003c6410ac73ad9
Max <max.fyall@hotmmail.co.uk>
2019-10-16 15:22:19 +0100

    removed spaces between equals signs

--- source/scripts/tar.sh
+++ source/scripts/tar.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Puts a version of a repository in a compressed file
 
-target = $1 #path to target repo
+target=$1 #path to target repo
 
 echo "Do you want to revert changes in this repository? (y/n)" # ask user if they want to revert the changes in the repo
 read userInput # reads the input from the user
 
-if [[ "$userInput" != "y" ]] || [[ "$userInput" != "n" ]]; then # check if the input is invalid
+if [[ "$userInput"!="y" ]] || [[ "$userInput"!="n" ]]; then # check if the input is invalid
 	
 	echo "INVALID INPUT, Please try again" # output error message if input is invalid
 
-else if [[ userInput == y ]]; then # check if the user wants to revert the changes
+else if [[ userInput==y ]]; then # check if the user wants to revert the changes
 
-	copyRepo = $(cp -r ($target) archivedRepo) # copys the repo needed
+	copyRepo=$(cp -r ($target) archivedRepo) # copys the repo needed
 
 	bash undochange.sh # call the undo changes method
 
@@ -23,7 +23,7 @@ else if [[ userInput == y ]]; then # check if the user wants to revert the chang
 
 	else
 
-		copyRepo = $(cp -r ($target) archivedRepo) # copys the repo needed
+		copyRepo=$(cp -r ($target) archivedRepo) # copys the repo needed
 
 		tar -cz copyRepo # archives it using tar command
 		tar -t copyRepo.tar # lists the contents of the tar file
