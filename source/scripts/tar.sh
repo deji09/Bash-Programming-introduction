cc9347cc4eff741843a75dc48276d102ea82de41
Max <max.fyall@hotmmail.co.uk>
2019-10-16 16:00:19 +0100

    Fixed some bugs. Still some command bugs present.

--- source/scripts/tar.sh
+++ source/scripts/tar.sh
@@ -5,14 +5,14 @@ target=$1 #path to target repo
 
 echo "Do you want to revert changes in this repository? (y/n)" # ask user if they want to revert the changes in the repo
 read userInput # reads the input from the user
-
-if [[ "$userInput"!="y" ]] || [[ "$userInput"!="n" ]]; then # check if the input is invalid
+echo $userInput
+if [[ "$userInput" != "y" ]] && [[ "$userInput" != "n" ]]; then # check if the input is invalid
 	
 	echo "INVALID INPUT, Please try again" # output error message if input is invalid
 
-else if [[ userInput==y ]]; then # check if the user wants to revert the changes
+elif [[ "$userInput" == "y" ]]; then # check if the user wants to revert the changes
 
-	copyRepo=$(cp -r ($target) archivedRepo) # copys the repo needed
+	copyRepo=$(cp -r $target archivedRepo) # copys the repo needed
 
 	bash undochange.sh # call the undo changes method
 
@@ -21,15 +21,11 @@ else if [[ userInput==y ]]; then # check if the user wants to revert the changes
 
 	rm -r archivedRepo # deletes the copied repo
 
-	else
+else
+	copyRepo=$(cp -r $target archivedRepo) # copys the repo needed
 
-		copyRepo=$(cp -r ($target) archivedRepo) # copys the repo needed
+	tar -cz copyRepo # archives it using tar command
+	tar -t copyRepo.tar # lists the contents of the tar file
 
-		tar -cz copyRepo # archives it using tar command
-		tar -t copyRepo.tar # lists the contents of the tar file
-
-		rm -r archivedRepo # deletes the copied repo
-
-	fi
-
+	rm -r archivedRepo # deletes the copied repo
 fi
