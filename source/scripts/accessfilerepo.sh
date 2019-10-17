821605d1b1b8b02e7098479a6bdcc81a12706399
Max Kelly <maxwill.kelly@gmail.com>
2019-10-18 00:39:36 +0100

    Adds success messages. updates errors

--- source/scripts/accessfilerepo.sh
+++ source/scripts/accessfilerepo.sh
@@ -3,9 +3,14 @@
 
 # Sets the repository the user want to access as imported from the main script
 target=$1
-# Changes into this repository
-cd $target
-# Lists all the files and folders in this repository using recursion
-ls -R $target
-# Starts a new shell, so the changed directory is applied
-exec $SHELL
+if [ -e $target ]; then
+    # Changes into this repository
+    cd $target
+    # Lists all the files and folders in this repository using recursion
+    ls -R $target
+    # Starts a new shell, so the changed directory is applied
+    exec $SHELL 
+else 
+    # Displays an error message
+    echo "Error: Repository doesn't exist"
+fi
