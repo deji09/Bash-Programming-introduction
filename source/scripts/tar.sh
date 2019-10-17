db3d6b0f6b853bbbcda1c6c47e4849bc9798dda2
Max <max.fyall@hotmmail.co.uk>
2019-10-17 21:42:33 +0100

    Added validation to tar method

--- source/scripts/tar.sh
+++ source/scripts/tar.sh
@@ -3,12 +3,16 @@
 
 target=$1 # path to target repo
 
-repoName=$(basename $target) # get the name of the repository
+if [[ "$target" == "null" ]]; then # check if the repo user aims to compress is null
+	echo "Error: Invalid Input" # if so then print an error code
+else # if not then continue witht the method
+	repoName=$(basename $target) # get the name of the repository
 
-cp -r $target $repoName # copys the repo needed
+	cp -r $target $repoName # copys the repo needed
 
-echo "Your compressed repository contains:" # print message
+	echo "Your compressed repository contains:" # print message
 
-tar -czf archivedRepo.tar.gz $repoName # archives it using tar command
-tar -tf archivedRepo.tar.gz # lists contents of the tar file
-rm -r $repoName # deletes the copied repos
+	tar -czf archivedRepo.tar.gz $repoName # archives it using tar command
+	tar -tf archivedRepo.tar.gz # lists contents of the tar file
+	rm -r $repoName # deletes the copied repos
+fi
