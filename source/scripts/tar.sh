db69e2b342854e176f6d0d555accaf5c829f1b38
Max <max.fyall@hotmmail.co.uk>
2019-10-17 17:40:57 +0100

    Improved tar method

--- source/scripts/tar.sh
+++ source/scripts/tar.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # Puts a version of a repository in a compressed file
 
-target=$1 #path to target repo
-trackproPath=$2 # path to the destination of the archived repo
+target=$1 # path to target repo
 
 repoName=$(basename $target) # get the name of the repository
 
-cp -r $target $trackproPath # copys the repo needed
+cp -r $target $repoName # copys the repo needed
 
-echo "Your compressed repository contains:"
+echo "Your compressed repository contains:" # print message
 
 tar -czf archivedRepo.tar.gz $repoName # archives it using tar command
 tar -tf archivedRepo.tar.gz # lists contents of the tar file
-rm -r $repoName # deletes the copied repo
+rm -r $repoName # deletes the copied repos
