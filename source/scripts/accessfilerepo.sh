95cd07664f023eff93a989cc1a87e32464749a3c
Max Kelly <maxwill.kelly@gmail.com>
2019-10-17 16:20:56 +0100

    Comments accessfilerepo

--- source/scripts/accessfilerepo.sh
+++ source/scripts/accessfilerepo.sh
@@ -1,6 +1,11 @@
 #!bin/bash
 # Allows the user to access a repository by changing into it
 
-cd $1
-ls -R $1
+# Sets the repository the user want to access as imported from the main script
+target=$1
+# Changes into this repository
+cd $target
+# Lists all the files and folders in this repository using recursion
+ls -R $target
+# Starts a new shell, so the changed directory is applied
 exec $SHELL
