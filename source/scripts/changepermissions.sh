064121974d8a79d138329de5b15ee2e1faf6afe3
Max <max.fyall@hotmmail.co.uk>
2019-10-18 12:18:45 +0100

    changed permissions file. still not working

--- source/scripts/changepermissions.sh
+++ source/scripts/changepermissions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # changes the permissions of every file stored
-# NOT IMPLEMENTED
 
 target=$1 # gets the repo user wants to edit permissions
 
@@ -9,7 +8,7 @@ repoName=$(basename $target) # get the name of the repo from the file path
 if [[ "$target" == null ]]; then # check if the user has not entererd a repo to edit
 	echo "Error: Invalid Input" # output an error message if true
 else
-	echo "WARNING: Permissions will be changed for group and other, not user, for the repository and all files inside" # warning message
+	echo "WARNING: Permissions will be changed for group and other, not user, for the repository" # warning message
 	echo "Do you wish to add or remove permissions? Enter A or R" # does user wish to add permissions or remove them from a repo
 	read line # get user input
 
@@ -24,18 +23,18 @@ else
 		fi
 
 		if [[ "$userInput" == "W" ]] || [[ "usertInput" == "w" ]]; then # if user wants to add write permissions
-			chmod -r g+w $target # add write permissions to group
-			chomd -r o+w $target # add write permissions to other
+			chmod g+w $target # add write permissions to group
+			chomd o+w $target # add write permissions to other
 		fi
 
 		if [[ "$userInput" == "R" ]] || [[ "$userInput" == "r" ]]; then # if user wants to add read permissions
-			chmod -r g+r $target # add read permissions to group
-			chmod -r o+r $target # add read permissions to other
+			chmod g+r $target # add read permissions to group
+			chmod o+r $target # add read permissions to other
 		fi
 
 		if [[ "$userInput" == "X" ]] || [[ "$userInput" == "x" ]]; then # if user wants to add execute permissions
-			chmod -r g+x $target # add execute permissions to group
-			chmod -r o+x $target # add execute permissions to other
+			chmod g+x $target # add execute permissions to group
+			chmod o+x $target # add execute permissions to other
 		fi
 	fi
 
@@ -50,18 +49,18 @@ else
 		fi
 
 		if [[ "$userInput2" == "W" ]] || [[ "usertInput" == "w" ]]; then # if user wants to remove write permissions
-			chmod -r g-w $target # remove write permissions from group
-			chmod -r o-w $target # remove write permissions from other
+			chmod g-w $target # remove write permissions from group
+			chmod o-w $target # remove write permissions from other
 		fi
 
 		if [[ "$userInput2" == "R" ]] || [[ "$userInput" == "r" ]]; then # if user wants to remove read permissions
-			chmod -r g-r $target # remove read permissions from group
-			chmod -r o-r $target # remove read permissions from other
+			chmod g-r $target # remove read permissions from group
+			chmod o-r $target # remove read permissions from other
 		fi
 
 		if [[ "$userInput2" == "X" ]] || [[ "$userInput" == "x" ]]; then # if user wants to remove execute permissions
-			chmod -r g-x $target # remove execute permissions from group
-			chmod -r o-x $target # remove execute permissions from other
+			chmod g-x $target # remove execute permissions from group
+			chmod o-x $target # remove execute permissions from other
 		fi
 	fi
 fi
